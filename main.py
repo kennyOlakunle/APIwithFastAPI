f7b82186a0b9a00d979b3fe074526de896784825
Kehinde Abe <olakunlemuyiwa@gmail.com>
2024-03-07 05:32:41 +0000

    making it work

--- main.py
+++ main.py
@@ -9,3 +9,23 @@ async def root():
 @app.get("/users")
 async def get_users():
     return {"users": {"user_1": "John", "user_2": "Jane", "user_3": "Doe"}}
+
+
+#Create, delete, and update users
+
+@app.post("/user")
+async def create_user(User):
+    # create user in a database
+    return {"message":"Created user with name {} created".format(User)}
+
+
+@app.delete("/user/{id}")
+async def delete_user(id:int):
+    # delete user in a database
+    return {"message":"User deleted with id {}".format(id)}
+
+
+@app.put("/user/{id}")
+async def update_user(id: int):
+    # update user in a database
+    return {"message":"User updated with id {}".format(id)}
